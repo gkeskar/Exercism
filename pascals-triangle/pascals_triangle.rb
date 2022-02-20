eca863323a1b676c4f6fa89cb86eedab469b384a
Gandhali Keskar <gandhali2001.yahoo.com>
2022-02-19 22:46:21 -0800

    Add private methods to extract logic to create new rows
    - method to create new elements of next row
      new elements are all the elements of row except leftmost(head) and rightmost(tail)
    - method to construct a row with prepending head(first element of last row) and append tail ( last element of last row )
      to new elements

--- pascals-triangle/pascals_triangle.rb
+++ pascals-triangle/pascals_triangle.rb
@@ -8,6 +8,17 @@ class Triangle
     @number_of_rows = number_of_rows
   end
 
+  def new_row(head, new_elements, tail)
+    new_elements.prepend(head).append(tail)
+  end
+
+  def new_elements(last_row)
+    number_of_elements = last_row.size - 2
+    0.upto(number_of_elements).each_with_object([]).with_index do |(_, array), index|
+       array << last_row[index] + last_row[index + 1]
+    end
+  end
+
   public
 
   def rows
@@ -16,15 +27,7 @@ class Triangle
         result << [1]
       else
         last_row = result[row - 2]
-        index = 0
-        next_row = []
-        next_row[0] = last_row.last
-        0.upto(last_row.count - 2).each do |index|
-          next_row << last_row[index] + last_row[index + 1]
-          index =+ 1
-        end
-        next_row << last_row.last
-        result << next_row
+        result << new_row(last_row.first, new_elements(last_row), last_row.last )
       end
     end
   end
