4125810e9affb4f038fe326a42c3263f4e3ceba6
Gandhali Keskar <gandhali2001.yahoo.com>
2022-02-26 15:29:49 -0800

    - Naming changes for variable, methods and class
    -  Naming changes to refect what they represents, what data they hold
    -  Refactor code to use min_factor and max_factor vs bounds in Palindromes Class
    -  Add Spaces after brackets in code blocks
    -  reorganize private and public methods in PalindromeAccessor class
    -  Add memoization for largest, smallest and generate methods

--- palindrome-products/palindrome_products.rb
+++ palindrome-products/palindrome_products.rb
@@ -1,11 +1,11 @@
-class PalindromeFactor
-
-  attr_reader :value, :factors
+class PalindromeAccessor
 
   def initialize(value, factors)
     @value = value
     @factors = [factors]
   end
+
+  attr_reader :value, :factors
 
   def add_factors(factor)
     factors << factor
@@ -17,43 +17,41 @@ class Palindromes
 
   private
 
-  attr_reader :bounds, :range
-
-  def initialize(bounds)
-    @bounds = bounds
-    max_factor = bounds[:max_factor]
-    bounds.key?(:min_factor) ? min_factor = bounds[:min_factor] : min_factor = 1
-    range = min_factor.upto(max_factor).to_a
-    @range = range
-    @palindrome_factors = []
+  def initialize(min_factor: 1, max_factor:)
+    @range = min_factor.upto(max_factor).to_a
+    @factors = []
   end
 
-  def palindrome?(number)
+  def is_palindrome?(number)
     number.digits == number.digits.reverse
   end
 
-  def pf_with_value(value)
-    @palindrome_factors.find{|pf| pf.value == value}
+  def palindrome_has_a_value(value)
+    @factors.find{ |palindrome| palindrome.value == value }
+  end
+
+  def generate_palindromes
+    @range.repeated_combination(2).each_with_object([]) do |combination, result|
+      product = combination.reduce(:*)
+      if  is_palindrome?(product)
+        found = palindrome_has_a_value(product)
+        found ? found.add_factors(combination) : @factors << PalindromeAccessor.new(product, combination)
+      end
+    end
   end
 
   public
 
   def largest
-    @palindrome_factors.sort_by{|pf| pf.value}.last
+    @largest ||= @factors.sort_by{ |palindrome| palindrome.value }.last
   end
 
   def smallest
-    @palindrome_factors.sort_by{|pf| pf.value}.first
+    @smallest ||= @factors.sort_by{ |palindrome| palindrome.value }.first
   end
 
   def generate
-    range.repeated_combination(2).each_with_object([]) do |array, result|
-      product = array.reduce(:*)
-      if  palindrome?(product)
-        already_found = pf_with_value(product)
-        already_found ? already_found.add_factors(array) : @palindrome_factors << PalindromeFactor.new(product, array)
-      end
-    end
+    @palindromes ||= generate_palindromes
   end
 
 end
