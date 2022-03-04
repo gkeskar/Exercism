20bceb76c7684854af3250657c62e5e8f23bba77
Gandhali Keskar <gandhali2001.yahoo.com>
2022-03-03 23:30:21 -0800

    Use case statement
    Use NEW_LINE constant

--- food-chain/food_chain.rb
+++ food-chain/food_chain.rb
@@ -1,5 +1,8 @@
 class FoodChain
-  require 'byebug'
+
+  NEW_LINE = "\n"
+
+  OPENING_LINE = "I know an old lady who swallowed a"
 
   ANIMALS = {
     1 => 'fly',  2 => 'spider', 
@@ -21,39 +24,41 @@ class FoodChain
 
   def self.song
     (1..8).map do |number_of_stanzas|
-      stanza(number_of_stanzas) + "\n"
+      stanza(number_of_stanzas) + NEW_LINE
     end.join
   end
 
   def self.first_two_lines(number)
     animal = ANIMALS[number]
     action = ACTION[animal]
-    "I know an old lady who swallowed a #{animal}.\n#{action}"
+    "#{OPENING_LINE} #{animal}." + NEW_LINE + "#{action}"
   end
 
-  def self.previous_lines(number)
-    stanza(number - 1).gsub("\n" + first_two_lines(number - 1),"")
-  end
-
-  def self.stanza(number)
-    if number == 1
-      first_two_lines(1)
-    elsif number == 8
-      "\n" + first_two_lines(8)
-    elsif number == 2
-      "\n" + first_two_lines(number) + new_line(number) + "\n#{ACTION['fly']}"
-    else
-      "\n"  + first_two_lines(number) + new_line(number) + previous_lines(number)
-    end
-  end
-
-  def self.new_line(number)
+  def self.third_line(number)
     animal = ANIMALS[number]
     previous_animal = ANIMALS[number - 1]
     action = ACTION[animal]
     previous_animal == 'spider' ?
-      line2 = "\nShe swallowed the #{animal} to catch the spider that wriggled and jiggled and tickled inside her."
-      : line2 = "\nShe swallowed the #{animal} to catch the #{previous_animal}."
+      "\nShe swallowed the #{animal} to catch the spider that wriggled and jiggled and tickled inside her."
+      : "\nShe swallowed the #{animal} to catch the #{previous_animal}."
+  end
+
+  def self.all_other_lines(number)
+    stanza(number - 1).gsub(NEW_LINE + first_two_lines(number - 1),"")
+  end
+
+  def self.stanza(number)
+    case number
+
+    when 1
+      first_two_lines(1)
+    when 8
+      NEW_LINE + first_two_lines(8)
+    when 2
+      NEW_LINE + first_two_lines(number) + third_line(number) + "\n#{ACTION['fly']}"
+    else
+      NEW_LINE  + first_two_lines(number) + third_line(number) + all_other_lines(number)
+    end
   end
 
 end
