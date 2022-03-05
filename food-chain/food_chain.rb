5d61470c842dd95b50b0dcc7a123d9589d65c1a8
Gandhali Keskar <gandhali2001.yahoo.com>
2022-03-04 16:10:52 -0800

    Use of string templates for concatination

--- food-chain/food_chain.rb
+++ food-chain/food_chain.rb
@@ -1,8 +1,6 @@
 class FoodChain
 
-  NEW_LINE = "\n"
-
-  OPENING_LINE = "I know an old lady who swallowed a"
+  OPENING_PHRASE = "I know an old lady who swallowed a"
 
   ANIMALS = {
     1 => 'fly',  2 => 'spider', 
@@ -13,7 +11,7 @@ class FoodChain
 
   ACTION = {
     'fly' => "I don't know why she swallowed the fly. Perhaps she'll die.",
-    'spider' => "It wriggled and jiggled and tickled inside her.",
+    'spider' => "wriggled and jiggled and tickled inside her.",
     'bird' => "How absurd to swallow a bird!",
     'cat' => "Imagine that, to swallow a cat!",
     'dog' => "What a hog, to swallow a dog!",
@@ -24,27 +22,29 @@ class FoodChain
 
   def self.song
     (1..8).map do |number_of_stanzas|
-      stanza(number_of_stanzas) + NEW_LINE
+      "%s\n" % [stanza(number_of_stanzas)]
     end.join
   end
 
   def self.first_two_lines(number)
     animal = ANIMALS[number]
-    action = ACTION[animal]
-    "#{OPENING_LINE} #{animal}." + NEW_LINE + "#{action}"
+    animal == 'spider' ?
+      action = "It " + ACTION['spider'] : action = ACTION[animal]
+    "%s %s.\n%s" % [OPENING_PHRASE, animal, action]
   end
 
   def self.third_line(number)
     animal = ANIMALS[number]
     previous_animal = ANIMALS[number - 1]
     action = ACTION[animal]
+    opening_phrase = "She swallowed the #{animal} to catch the #{previous_animal}"
+    spider_phrase = "that #{ACTION['spider']}"
     previous_animal == 'spider' ?
-      "\nShe swallowed the #{animal} to catch the spider that wriggled and jiggled and tickled inside her."
-      : "\nShe swallowed the #{animal} to catch the #{previous_animal}."
+      "\n%s %s" % [opening_phrase, spider_phrase] : "\n%s." % [opening_phrase]
   end
 
-  def self.all_other_lines(number)
-    stanza(number - 1).gsub(NEW_LINE + first_two_lines(number - 1),"")
+  def self.cumulative_lines(number)
+    stanza(number - 1).gsub( "\n%s" % [first_two_lines(number - 1)],"")
   end
 
   def self.stanza(number)
@@ -53,11 +53,11 @@ class FoodChain
     when 1
       first_two_lines(1)
     when 8
-      NEW_LINE + first_two_lines(8)
+      "\n%s" % [first_two_lines(8)]
     when 2
-      NEW_LINE + first_two_lines(number) + third_line(number) + "\n#{ACTION['fly']}"
+      "\n%s" "%s" "\n%s" % [first_two_lines(2), third_line(2), ACTION['fly']]
     else
-      NEW_LINE  + first_two_lines(number) + third_line(number) + all_other_lines(number)
+       "\n%s" "%s" "%s" % [first_two_lines(number), third_line(number), cumulative_lines(number)]
     end
   end
 
