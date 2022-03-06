4ef626f2f1ca4cfc23224352f2473b3fe962e2fa
Gandhali Keskar <gandhali2001.yahoo.com>
2022-03-05 16:56:28 -0800

    Use Array Vs Hash to store animals with keys
    Use variable for total number of stanzas
    Use ternary operator as expression

--- food-chain/food_chain.rb
+++ food-chain/food_chain.rb
@@ -2,12 +2,7 @@ class FoodChain
 
   OPENING_PHRASE = "I know an old lady who swallowed a"
 
-  ANIMALS = {
-    1 => 'fly',  2 => 'spider', 
-    3 => 'bird', 4 => 'cat', 
-    5 => 'dog',  6 => 'goat',
-    7 => 'cow',  8 => 'horse'
-  }.freeze
+  ANIMALS = [ 'fly', 'spider', 'bird', 'cat', 'dog', 'goat','cow', 'horse' ]
 
   ACTION = {
     'fly' => "I don't know why she swallowed the fly. Perhaps she'll die.",
@@ -18,18 +13,18 @@ class FoodChain
     'goat' => "Just opened her throat and swallowed a goat!",
     'cow' => "I don't know how she swallowed a cow!",
     'horse' => "She's dead, of course!"
-  }.freeze
+  }
 
   def self.song
-    (1..8).map do |number_of_stanzas|
+     total_stanzas = 8
+    (0..total_stanzas - 1).map do |number_of_stanzas|
       "%s\n" % [stanza(number_of_stanzas)]
     end.join
   end
 
   def self.first_two_lines(number)
     animal = ANIMALS[number]
-    animal == 'spider' ?
-      action = "It " + ACTION['spider'] : action = ACTION[animal]
+      action = animal == 'spider' ? "It " + ACTION['spider'] : action = ACTION[animal]
     "%s %s.\n%s" % [OPENING_PHRASE, animal, action]
   end
 
@@ -50,12 +45,12 @@ class FoodChain
   def self.stanza(number)
     case number
 
+    when 0
+      first_two_lines(0)
+    when 7
+      "\n%s" % [first_two_lines(7)]
     when 1
-      first_two_lines(1)
-    when 8
-      "\n%s" % [first_two_lines(8)]
-    when 2
-      "\n%s" "%s" "\n%s" % [first_two_lines(2), third_line(2), ACTION['fly']]
+      "\n%s" "%s" "\n%s" % [first_two_lines(1), third_line(1), ACTION['fly']]
     else
        "\n%s" "%s" "%s" % [first_two_lines(number), third_line(number), cumulative_lines(number)]
     end
