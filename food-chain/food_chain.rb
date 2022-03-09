9330db655a8a83f5de5be0017053604b2e04c0e1
Gandhali Keskar <gandhali2001.yahoo.com>
2022-03-08 23:35:22 -0800

    cumulative_lines method refactor
    - No longer third_lines method , `cumulative_lines` generates all lines except for first two lines
      including 3rd line
    - Identify private methods and public methods
    - Use templete strings in `cumulative_lines` methods to generate third line
    - Third line has a check to handle special conditions for animals that has annotations (third_line_suffix)
    - ANIMALS_AND_ACTIONS hash has human redable tabular form
    - `puts FoodChain.new` calls to_s method automatically so no need to call FoodChain.song
    - `to_s` method refactor to use ANIMALS_AND_ACTIONS.length.times loop through all animals and actions instead of hardcoding 0..7

--- food-chain/food_chain.rb
+++ food-chain/food_chain.rb
@@ -1,28 +1,24 @@
 class FoodChain
 
     OPENING_PHRASE = "I know an old lady who swallowed a"
-
+    ANIMAL_PHRASE_PREFIX = 'She swallowed the %<animal>s to catch the %<prior_animal>s'
+    ANIMAL_PHRASE_SUFFIX = 'that %<animal_action>s'
     ANIMALS_AND_ACTIONS = {
-      'fly' => "I don't know why she swallowed the fly. Perhaps she'll die.",
-      'spider' => "wriggled and jiggled and tickled inside her.",
-      'bird' => "How absurd to swallow a bird!",
-      'cat' => "Imagine that, to swallow a cat!",
-      'dog' => "What a hog, to swallow a dog!",
-      'goat' => "Just opened her throat and swallowed a goat!",
-      'cow' => "I don't know how she swallowed a cow!",
-      'horse' => "She's dead, of course!"
+      'fly'     => "I don't know why she swallowed the fly. Perhaps she'll die.",
+      'spider'  => "wriggled and jiggled and tickled inside her.",
+      'bird'    => "How absurd to swallow a bird!",
+      'cat'     => "Imagine that, to swallow a cat!",
+      'dog'     => "What a hog, to swallow a dog!",
+      'goat'    => "Just opened her throat and swallowed a goat!",
+      'cow'     => "I don't know how she swallowed a cow!",
+      'horse'   => "She's dead, of course!"
     }
 
     def self.song
       new.to_s
     end
 
-    def to_s
-      (0..7).each_with_object("") do |number_of_stanzas, lines|
-        lines << "%s\n\n" % [stanza(number_of_stanzas)]
-      end.chomp
-    end
-
+    private
 
     def first_two_lines(number)
       animal = ANIMALS_AND_ACTIONS.keys[number]
@@ -30,32 +26,42 @@ class FoodChain
       "%s %s.\n%s" % [OPENING_PHRASE, animal, action]
     end
 
-    def third_line(number)
-      return "" if ( number == 0 || number == 7 )
-      animal = ANIMALS_AND_ACTIONS.keys[number]
-      previous_animal = ANIMALS_AND_ACTIONS.keys[number - 1]
-      action = ANIMALS_AND_ACTIONS[animal]
-      opening_phrase = "She swallowed the #{animal} to catch the #{previous_animal}"
-      spider_phrase = "that #{ANIMALS_AND_ACTIONS['spider']}"
-      previous_animal == 'spider' ?
-        "\n%s %s" % [opening_phrase, spider_phrase] : "\n%s." % [opening_phrase]
+    def third_line_suffix_for_animal?(animal)
+       animal == 'spider'
+    end
+
+    def third_line_suffix(animal)
+      ANIMAL_PHRASE_SUFFIX % {animal_action: ANIMALS_AND_ACTIONS[animal]}
     end
 
     def cumulative_lines(number)
-      return "" if ( number == 0 || number == 7 )
-      return  "\n%s" % [ANIMALS_AND_ACTIONS['fly']] if number == 1
-      lines = number.downto(2).each_with_object("") do | number_of_stanzas, result |
-        result << third_line(number_of_stanzas - 1)
+      return "" if  number == 0 || number == 7
+      lines = number.downto(1).each_with_object("") do | number_of_stanzas, result |
+        animal = ANIMALS_AND_ACTIONS.keys[number_of_stanzas]
+        previous_animal = ANIMALS_AND_ACTIONS.keys[number_of_stanzas - 1]
+        action = ANIMALS_AND_ACTIONS[animal]
+        third_line_prefix = ANIMAL_PHRASE_PREFIX % {animal: animal, prior_animal: previous_animal}
+        third_line_suffix_for_animal?(previous_animal) ?
+          third_line = "%s %s" % [third_line_prefix,third_line_suffix(previous_animal)] : third_line = third_line_prefix
+        result << "\n%s." % [third_line.chomp('.')]
       end
       "%s\n%s" % [lines, ANIMALS_AND_ACTIONS['fly']]
     end
 
     def stanza(number)
-      first_two_lines(number) + third_line(number) + cumulative_lines(number)
+      first_two_lines(number) << cumulative_lines(number)
+    end
+
+    public
+
+    def to_s
+      ANIMALS_AND_ACTIONS.length.times.each_with_object("") do |number_of_stanzas, lines|
+        lines << "%s\n\n" % [stanza(number_of_stanzas)]
+      end.chomp
     end
 
 end
 
 if $PROGRAM_NAME == __FILE__
-  puts FoodChain.song
+  puts FoodChain.new
 end
