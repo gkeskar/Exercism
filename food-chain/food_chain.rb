306f839f4f700cfc5ba2172639639fa180623e2f
Gandhali Keskar <gandhali2001.yahoo.com>
2022-03-10 22:57:48 -0800

    Simplify cumulative_lines method by refactoring third_line_suffix, rewrite each_with_object
    new method third_line_suffix(prior_animal) returns necessary string when animal == spider
    This simplies cumulative_lines just by calling one method third_line_suffix(prior_animal)
    
    each_with_object in cummulative_lines builts the object outside of the block.
    
    Change the name in the block argument list from number_of_stanzas to simply stanza
    
    Use `sub` vs `gsub` o change first occurance of "It" to "that"
    
    Define a method (instead of an assignment) named animal and give the number to retrieve it.
      same for action to avoid repetations

--- food-chain/food_chain.rb
+++ food-chain/food_chain.rb
@@ -1,9 +1,7 @@
 class FoodChain
 
     OPENING_PHRASE = "I know an old lady who swallowed a"
-
     ANIMAL_PHRASE_PREFIX = 'She swallowed the %<animal>s to catch the %<prior_animal>s'
-
     ANIMALS_AND_ACTIONS = {
       'fly'     => "I don't know why she swallowed the fly. Perhaps she'll die.",
       'spider'  => "It wriggled and jiggled and tickled inside her.",
@@ -21,31 +19,28 @@ class FoodChain
 
     private
 
-    def first_two_lines(number)
-      animal = ANIMALS_AND_ACTIONS.keys[number]
-      action = ANIMALS_AND_ACTIONS[animal]
-      "%s %s.\n%s" % [OPENING_PHRASE, animal, action]
+    def animal(number)
+      ANIMALS_AND_ACTIONS.keys[number]
     end
 
-    def third_line_suffix_for_animal?(animal)
-       animal == 'spider'
+    def action(number)
+      ANIMALS_AND_ACTIONS[animal(number)]
+    end
+
+    def first_two_lines(number)
+      "%s %s.\n%s" % [OPENING_PHRASE, animal(number), action(number)]
     end
 
     def third_line_suffix(animal)
-      return ANIMALS_AND_ACTIONS[animal].gsub("It","that") if animal == 'spider'
+      ANIMALS_AND_ACTIONS[animal].sub("It","that") if animal == 'spider'
     end
 
     def cumulative_lines(number)
       return "" if number == 0 || number == ANIMALS_AND_ACTIONS.length - 1
-      lines = number.downto(1).each_with_object("") do | number_of_stanzas, result |
-        animal = ANIMALS_AND_ACTIONS.keys[number_of_stanzas]
-        prior_animal = ANIMALS_AND_ACTIONS.keys[number_of_stanzas - 1]
-        action = ANIMALS_AND_ACTIONS[animal]
-        second_line = (number)
-        third_line_prefix = ANIMAL_PHRASE_PREFIX % {animal: animal, prior_animal: prior_animal}
-        third_line_suffix_for_animal?(prior_animal) ?
-          third_line = "%s %s" % [third_line_prefix,third_line_suffix(prior_animal)] : third_line = third_line_prefix
-        result << "\n%s." % [third_line.chomp('.')]
+      lines = number.downto(1).each_with_object(lines = "") do | stanza |
+        third_line_prefix = ANIMAL_PHRASE_PREFIX % {animal: animal(stanza), prior_animal: animal(stanza - 1)}
+        third_line = "%s %s" % [third_line_prefix,third_line_suffix(animal(stanza - 1))]
+        lines << "\n%s." % [third_line.strip.chomp('.')]
       end
       "%s\n%s" % [lines, ANIMALS_AND_ACTIONS['fly']]
     end
