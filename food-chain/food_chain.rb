0039eb8debc15657a014f4600a3ecd2cd5b229b5
Gandhali Keskar <gandhali2001.yahoo.com>
2022-03-11 15:49:39 -0800

    Use structs (vs Hash) to keep data for animals and actions
    ANIMALS is now array of Animal structs with attributes as type and action.
    Array tells us that the order of elements is important.

--- food-chain/food_chain.rb
+++ food-chain/food_chain.rb
@@ -2,16 +2,17 @@ class FoodChain
 
     OPENING_PHRASE = "I know an old lady who swallowed a"
     ANIMAL_PHRASE_PREFIX = 'She swallowed the %<animal>s to catch the %<prior_animal>s'
-    ANIMALS_AND_ACTIONS = {
-      'fly'     => "I don't know why she swallowed the fly. Perhaps she'll die.",
-      'spider'  => "It wriggled and jiggled and tickled inside her.",
-      'bird'    => "How absurd to swallow a bird!",
-      'cat'     => "Imagine that, to swallow a cat!",
-      'dog'     => "What a hog, to swallow a dog!",
-      'goat'    => "Just opened her throat and swallowed a goat!",
-      'cow'     => "I don't know how she swallowed a cow!",
-      'horse'   => "She's dead, of course!"
-    }
+    Animal = Struct.new(:type, :action)
+    ANIMALS = [
+      Animal.new("fly", "I don't know why she swallowed the fly. Perhaps she'll die."),
+      Animal.new("spider", "It wriggled and jiggled and tickled inside her."),
+      Animal.new("bird", "How absurd to swallow a bird!"),
+      Animal.new("cat", "Imagine that, to swallow a cat!"),
+      Animal.new("dog", "What a hog, to swallow a dog!"),
+      Animal.new("goat", "Just opened her throat and swallowed a goat!"),
+      Animal.new("cow", "I don't know how she swallowed a cow!"),
+      Animal.new("horse", "She's dead, of course!"),
+    ]
 
     def self.song
       new.to_s
@@ -20,15 +21,19 @@ class FoodChain
     private
 
     def animal(number)
-      ANIMALS_AND_ACTIONS.keys[number]
+      ANIMALS[number].type
     end
 
     def prior_animal(number)
-      animal(number - 1)
+      ANIMALS[number - 1].type
     end
 
     def action(number)
-      ANIMALS_AND_ACTIONS[animal(number)]
+      ANIMALS[number].action
+    end
+
+    def prior_animal_action(number)
+      ANIMALS[number - 1].action
     end
 
     def first_two_lines(number)
@@ -37,12 +42,12 @@ class FoodChain
 
     def third_line(number)
       third_line_prefix = ANIMAL_PHRASE_PREFIX % {animal: animal(number), prior_animal: prior_animal(number)}
-      third_line_suffix = prior_animal(number) == 'spider' ? ANIMALS_AND_ACTIONS[prior_animal(number)].sub('It','that') : ''
+      third_line_suffix = prior_animal(number) == 'spider' ? prior_animal_action(number).sub('It','that') : ''
       ("%s %s" % [third_line_prefix,third_line_suffix]).strip.chomp('.')
     end
 
     def build_cumulative_lines?(number)
-      (1..ANIMALS_AND_ACTIONS.length - 2).include?(number)
+      (1..ANIMALS.length - 2).include?(number)
     end
 
     def cumulative_lines(number)
@@ -50,7 +55,7 @@ class FoodChain
       number.downto(1).each_with_object(lines = '') do | stanza |
         lines << "\n%s." % [third_line(stanza)]
       end
-      "%s\n%s" % [lines, ANIMALS_AND_ACTIONS['fly']]
+      "%s\n%s" % [lines, action(0)]
     end
 
     def stanza(number)
@@ -60,7 +65,7 @@ class FoodChain
     public
 
     def song
-      ANIMALS_AND_ACTIONS.length.times.each_with_object('') do |stanza, lines|
+      ANIMALS.length.times.each_with_object('') do |stanza, lines|
         lines << "%s\n\n" % [stanza(stanza)]
       end.chomp
     end
