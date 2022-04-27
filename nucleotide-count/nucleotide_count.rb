faa8ab0e7cb5e3e03b88e12c348cc9573ac81320
Gandhali Keskar <gandhali2001.yahoo.com>
2022-04-26 22:23:29 -0700

    Make empty_histogram as private constant and use empty_histogram.clone

--- nucleotide-count/nucleotide_count.rb
+++ nucleotide-count/nucleotide_count.rb
@@ -27,15 +27,14 @@ class Nucleotide
     sequence.chars.all?{|c| NUCLEOTIDES.include?(c)}
   end
 
-  def empty_histogram
-    { 'A' => 0, 'T' => 0, 'C' => 0, 'G' => 0 }
-  end
+  EMPTY_HISTOGRAM = { 'A' => 0, 'T' => 0, 'C' => 0, 'G' => 0 }
+  private_constant :EMPTY_HISTOGRAM
 
   public
 
   def histogram
-    return empty_histogram if sequence == ""
-    sequence.chars.inject(empty_histogram) do | output, nucleotide |
+    return EMPTY_HISTOGRAM if sequence.empty?
+    sequence.chars.inject(EMPTY_HISTOGRAM.clone) do | output, nucleotide |
       output[nucleotide] = sequence.count(nucleotide)
       output
     end
