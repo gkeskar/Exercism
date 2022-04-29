d0903fe9648947598e2174d969d2f40e0f6c1e0f
Gandhali Keskar <gandhali2001.yahoo.com>
2022-04-28 21:56:10 -0700

    Use each_with_object, fix spacing in hash and block, use %w for string array
    - each_with_object as we are now starting with fresh hostogram (empty) hash
    - fix spacing for ex. {no_space: value} and { # this is a block }
    - %w[A C G T] va ['A', 'C', 'G', 'T']

--- nucleotide-count/nucleotide_count.rb
+++ nucleotide-count/nucleotide_count.rb
@@ -8,7 +8,7 @@ end
 
 class Nucleotide
 
-  NUCLEOTIDES = ['A', 'C', 'G', 'T']
+  NUCLEOTIDES = %w[A C G T]
 
   def self.from_dna(sequence)
     new(sequence)
@@ -24,19 +24,18 @@ class Nucleotide
   end
 
   def valid?
-    sequence.chars.all?{|c| NUCLEOTIDES.include?(c)}
+    sequence.chars.all?{ |c| NUCLEOTIDES.include?(c) }
   end
 
-  EMPTY_HISTOGRAM = { 'A' => 0, 'T' => 0, 'C' => 0, 'G' => 0 }
+  EMPTY_HISTOGRAM = {'A' => 0, 'T' => 0, 'C' => 0, 'G' => 0}
   private_constant :EMPTY_HISTOGRAM
 
   public
 
   def histogram
     return EMPTY_HISTOGRAM if sequence.empty?
-    sequence.chars.inject(EMPTY_HISTOGRAM.clone) do | output, nucleotide |
+    sequence.chars.each_with_object(EMPTY_HISTOGRAM.clone) do | nucleotide,output |
       output[nucleotide] = sequence.count(nucleotide)
-      output
     end
   end
 
