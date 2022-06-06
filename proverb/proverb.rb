af1482582a215a594641620c84e4e7b170a76864
Gandhali Keskar <gandhali2001.yahoo.com>
2022-06-05 21:00:22 -0700

    Names to reflect closer to what is used, Use _ for unused block argument

--- proverb/proverb.rb
+++ proverb/proverb.rb
@@ -2,31 +2,31 @@ class Proverb
 
   private
 
-  attr_reader :objects, :qualifier
+  attr_reader :things, :qualifier
 
-  def initialize(*objects, qualifier: '')
-    @objects = objects
+  def initialize(*things, qualifier: '')
+    @things = things
     @qualifier = qualifier
   end
 
-  def cause_effect_pairs
-    objects.each.with_index.reduce([]) do |cause_effect_pair, (value, index)|
-      if objects.slice(index, 2).length == 2
-        cause_effect_pair  << objects.slice(index, 2)
+  def cause_and_effect
+    things.each_with_index.reduce([]) do |cause_effect_pair, (_, index)|
+      if things.slice(index, 2).length == 2
+        cause_effect_pair  << things.slice(index, 2)
       end
       cause_effect_pair
     end
   end
 
-  def phrases(object_pairs)
-    phrases = object_pairs.reduce("") do  |phrase, object_pair|
-      cause = object_pair.first
-      effect = object_pair.last
+  def phrases(pair_of_things)
+    phrases = pair_of_things.reduce("") do  |phrase, things|
+      cause = things.first
+      effect = things.last
       phrase << "For want of a %s the %s was lost.\n" % [cause, effect]
     end
-    first_object_of_chain = object_pairs.first.first
+    first_object_of_chain = pair_of_things.first.first
     if qualifier.empty? ? optional_qualifier = first_object_of_chain :
-        optional_qualifier = "%s %s" %[qualifier, first_object_of_chain]
+      optional_qualifier = "%s %s" %[qualifier, first_object_of_chain]
     end
     final_phrase = "And all for the want of a %s." %[optional_qualifier]
     phrases + final_phrase
@@ -35,7 +35,7 @@ class Proverb
   public
 
   def to_s
-    phrases(cause_effect_pairs)
+    phrases(cause_and_effect)
   end
 
 end
