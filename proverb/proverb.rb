f80ebc6adf73226680633e80a1dbe071bdbb1b13
Gandhali Keskar <gandhali2001.yahoo.com>
2022-06-07 00:00:15 -0700

    Use recursion to build repetative phrases

--- proverb/proverb.rb
+++ proverb/proverb.rb
@@ -1,5 +1,4 @@
 class Proverb
-
   private
 
   attr_reader :things, :qualifier
@@ -9,33 +8,29 @@ class Proverb
     @qualifier = qualifier
   end
 
-  def cause_and_effect
-    things.each_with_index.reduce([]) do |cause_effect_pair, (_, index)|
-      if things.slice(index, 2).length == 2
-        cause_effect_pair  << things.slice(index, 2)
-      end
-      cause_effect_pair
-    end
+  def repetative_pharses(things)
+    phrase = "For want of a %s the %s was lost.\n" % [things.first, things.last]
+    return phrase if things.length == 2
+    repetative_pharses(things.take(2)) + repetative_pharses(things.drop(1))
   end
 
-  def phrases(pair_of_things)
-    phrases = pair_of_things.reduce("") do  |phrase, things|
-      cause = things.first
-      effect = things.last
-      phrase << "For want of a %s the %s was lost.\n" % [cause, effect]
-    end
-    first_object_of_chain = pair_of_things.first.first
+  def final_phrase(qualifier)
+    first_object_of_chain = things.first
     if qualifier.empty? ? optional_qualifier = first_object_of_chain :
-      optional_qualifier = "%s %s" %[qualifier, first_object_of_chain]
+       optional_qualifier = "%s %s" %[qualifier, first_object_of_chain]
     end
-    final_phrase = "And all for the want of a %s." %[optional_qualifier]
-    phrases + final_phrase
+    "And all for the want of a %s." %[optional_qualifier]
+  end
+
+
+  def phrases(*things)
+    repetative_pharses(things) + final_phrase(qualifier)
   end
 
   public
 
   def to_s
-    phrases(cause_and_effect)
+    phrases(*things)
   end
 
 end
