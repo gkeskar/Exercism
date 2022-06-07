9a69d09847d860e221dc9dc2d4e73cc824f3f341
Gandhali Keskar <gandhali2001.yahoo.com>
2022-06-07 00:04:33 -0700

    Fix spelling mistake of word 'repetitive'

--- proverb/proverb.rb
+++ proverb/proverb.rb
@@ -8,23 +8,22 @@ class Proverb
     @qualifier = qualifier
   end
 
-  def repetative_pharses(things)
+  def repetitive_pharses(things)
     phrase = "For want of a %s the %s was lost.\n" % [things.first, things.last]
     return phrase if things.length == 2
-    repetative_pharses(things.take(2)) + repetative_pharses(things.drop(1))
+    repetitive_pharses(things.take(2)) + repetitive_pharses(things.drop(1))
   end
 
   def final_phrase(qualifier)
     first_object_of_chain = things.first
     if qualifier.empty? ? optional_qualifier = first_object_of_chain :
-       optional_qualifier = "%s %s" %[qualifier, first_object_of_chain]
+      optional_qualifier = "%s %s" %[qualifier, first_object_of_chain]
     end
     "And all for the want of a %s." %[optional_qualifier]
   end
 
-
   def phrases(*things)
-    repetative_pharses(things) + final_phrase(qualifier)
+    repetitive_pharses(things) + final_phrase(qualifier)
   end
 
   public
