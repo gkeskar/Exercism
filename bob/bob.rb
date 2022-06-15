4c1ab966badcbc5923ab85bdf14aae8471fdcf40
Gandhali Keskar <gandhali2001.yahoo.com>
2022-06-14 21:35:11 -0700

    Use regex for yelling instead of lamda
    (?=.*[A-Z]) checks whether there are capital letters in the remark. This is called a positive lookahead
    [^a-z]+$ checks that the whole remark doesn't have lowercase letters at all. that's called a negated character class

--- bob/bob.rb
+++ bob/bob.rb
@@ -1,13 +1,12 @@
 class Bob
 
   def self.hey(remark)
-    # require 'byebug';byebug
     remark = remark.gsub(/['\d\s]/,"")
     case remark
-    when /^[A-Z]+\?$/ then "Calm down, I know what I'm doing!"
-    when  /^[\w\D]*\?$/ then "Sure."
-    when -> (remark) { /^[A-Z]+$/.match(remark.gsub(/\W/,"")) } then "Whoa, chill out!"
-    when  /^$/ then "Fine. Be that way!"
+    when  /^[A-Z]+\?$/            then "Calm down, I know what I'm doing!"
+    when  /^[\w\D]*\?$/           then "Sure."
+    when  /^(?=.*[A-Z])[^a-z]+$/  then "Whoa, chill out!"
+    when  /^$/                    then "Fine. Be that way!"
     else
        "Whatever."
     end
